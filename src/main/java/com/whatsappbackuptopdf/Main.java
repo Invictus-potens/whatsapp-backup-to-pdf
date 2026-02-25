3f6dbe675d69839928c996345e362e46e39f99a0
Invictus-potens <felipeommelo@gmail.com>
2026-02-25 17:56:11 -0300

    FEAT: Adicionado uma logica de identificação de arquivos zip dentro do root onde o arquivo é executado, agora qualquer arquivo zip encontrado na pasta é extraido, removendo a necessidade do usuário final renomear o arquivo ou mudar o diretório ná variável responsavel.

--- src/main/java/com/whatsappbackuptopdf/Main.java
+++ src/main/java/com/whatsappbackuptopdf/Main.java
@@ -1,20 +1,32 @@
 package com.whatsappbackuptopdf;
+
 import com.whatsappbackuptopdf.extractor.ZipExtractor;
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         ZipExtractor extractor = new ZipExtractor();
-
-        String zipPath = "C:\\Users\\felip\\IdeaProjects\\whatsapp-backup-to-pdf\\whatsapp.zip";
+        File currentDir = new File (".");
         String destPath = "./output_folder";
-    try {
-        System.out.println("Starting");
-        extractor.extract(zipPath, destPath);
-        System.out.println("Process finished");
-    } catch (IOException e) {
-        System.err.println("Error " + e.getMessage());
-    }
 
+        File[] zipFiles = currentDir.listFiles((dir,name) -> name.toLowerCase().endsWith(".zip"));
+
+        if (zipFiles != null && zipFiles.length > 0) {
+            System.out.println("Found " + zipFiles.length + " zip files.");
+
+            for (File zip : zipFiles) {
+                try {
+                    System.out.println("Starting the extraction of " + zip.getName());
+                    extractor.extract(zip.getAbsolutePath(), destPath);
+                    System.out.println("Process finished for " +zip.getName());
+                } catch (IOException e) {
+                    System.err.println("Error extracting " + zip.getName() + ": " + e.getMessage());
+                }
+            }
+            System.out.println("Process finished. ");
+        }else {
+            System.out.println("No files found.");
+        }
     }
 }
